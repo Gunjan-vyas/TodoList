c15dc89b9d59c1d233815fc3098fc0792f8bab6e
Gunjan Vyas <gunjan.vyas@moneyview.in>
2024-10-06 00:58:40 +0530

    backend files updated

--- Backend/config.js
+++ Backend/config.js
@@ -1,7 +1,6 @@
+require("dotenv").config(); //required to get .env file config values
 module.exports = {
   db: {
-    url: "mongodb+srv://gunjan432udr:gunjan123@cluster0.7orqzc2.mongodb.net/todo", // Stage mongoDB address
-    //url: "mongodb://ip-13-234-204-91.ap-south-1.compute.internal:27017/cms",
-    //url: "mongodb://ip-172-31-26-211.ap-south-1.compute.internal:27017/cms",
+    url: process.env.MONGO_URI,
   },
 };
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// require('dotenv').config();
 
 const { createTodo, updateTodo } = require("./type");
 const { todo } = require("./db");
